8bb8a9d96e480d4c3ec010929c9007ba3cc50a56
Sandeep Singh <sandeep.singhh1469@gmail.com>
2023-05-07 03:14:30 -0400

    first commit

---
+++ src/main/codingninjas/CodingNinjas.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.codingninjas;
+/**
+ *
+ * @author ssand
+ */
+public class CodingNinjas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println(BinarySearchEx.binarySearch(new int[]{1,2,2,3,3,3,4,5,6,7,11}, 3));
+         System.out.println(normalBinarySearch.binarySearch(new int[]{1,2,3,4,5,6,7,11}, 3));
+    }
+}
