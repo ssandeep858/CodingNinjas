76c38ef37d47d49e1f2c86283b811277a421c729
Sandeep Singh <sandeep.singhh1469@gmail.com>
2023-05-10 03:14:41 -0400

    prime

--- src/test/java/CodingNinjas.java
+++ src/test/java/CodingNinjas.java
@@ -2,8 +2,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
  */
-
-package com.mycompany.codingninjas;
 /**
  *
  * @author ssand
@@ -12,7 +10,8 @@ public class CodingNinjas {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println(BinarySearchEx.binarySearch(new int[]{1,2,2,3,3,3,4,5,6,7,11}, 3));
-         System.out.println(normalBinarySearch.binarySearch(new int[]{1,2,3,4,5,6,7,11}, 3));
+        //System.out.println(BinarySearchEx.binarySearch(new int[]{1,2,2,3,3,3,4,5,6,7,11}, 3));
+        //System.out.println(normalBinarySearch.binarySearch(new int[]{1,2,3,4,5,6,7,11}, 3));
+        System.out.println(PrimeNumberOrNot.isPrime(12));
     }
 }
---
+++ src/test/java/Fibonacci.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author ssand
+ */
+public class Fibonacci {
+
+    public static void printFibonacciNumbers(int num) {
+        int first = 0, second = 1;
+        System.out.println(first);
+        System.out.println(second);
+        for (int i = 0; i <= num; i++) {
+
+            int sum = first + second;
+            if (sum >= 0 && sum <= num) {
+                System.out.println(sum);
+            }
+            first = second;
+            second = sum;
+        }
+    }
+
+    public static void main(String[] args) {
+        printFibonacciNumbers(7);
+    }
+// 0,1,1,2,3,5,8,13,21   
+}
