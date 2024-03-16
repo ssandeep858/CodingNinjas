16aee7ce9e31305541a15a158bd53251a378349a
Sandeep Singh <ssandeep858@yahoo.com>
2024-03-16 03:08:23 -0400

    updates

--- src/main/java/NeetCode150/Array/ListOrHashMaps/LongestRepeatingCharacterReplacement.java
+++ src/main/java/NeetCode150/Array/ListOrHashMaps/LongestRepeatingCharacterReplacement.java
@@ -10,8 +10,26 @@ import java.util.Map;
 /**
  *
  * @author ssingh
+ * string s and an integer k. You can choose any character of the
+ * string and change it to any other uppercase English character. You can
+ * perform this operation at most k times.
+ *
+ * Return the length of the longest substring containing the same letter you can
+ * get after performing the above operations.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: s = "ABAB", k = 2 Output: 4 Explanation: Replace the two 'A's with two
+ * 'B's or vice versa. Example 2:
+ *
+ * Input: s = "AABABBA", k = 1 Output: 4 Explanation: Replace the one 'A' in the
+ * middle with 'B' and form "AABBBBA". The substring "BBBB" has the longest
+ * repeating letters, which is 4. There may exists other ways to achieve this
+ * answer too.
  */
-public class characterReplacement {
+public class LongestRepeatingCharacterReplacement {
 
     public static int getMaxValue(HashMap<Character, Integer> hashMap) {
         int max = 0;
@@ -26,6 +44,7 @@ public class characterReplacement {
         HashMap<Character, Integer> hashMap = new HashMap<>();
         int left = 0, right = 0;
         while (right < s.length()) {
+            //// window length ---- count[maximum means how many we have to replace
             hashMap.put(s.charAt(right), hashMap.getOrDefault(s.charAt(right), 0) + 1);
 
             while (right - left + 1 - getMaxValue(hashMap) > k) {
@@ -35,9 +54,7 @@ public class characterReplacement {
 
             maxRange = Math.max(maxRange, right - left + 1);
             right++;
-
         }
-
         return maxRange;
     }
 
@@ -46,6 +63,8 @@ public class characterReplacement {
         HashMap<Character, Integer> hashMap = new HashMap<>();
         int left = 0, right = 0;
         while (right < s.length()) {
+
+            // window length ---- count[maximum means how many we have to replace
             hashMap.put(s.charAt(right), hashMap.getOrDefault(s.charAt(right), 0) + 1);
             if (right - left + 1 - getMaxValue(hashMap) <= k) {
                 System.out.println("max value right now is " + getMaxValue(hashMap));
@@ -65,6 +84,6 @@ public class characterReplacement {
     }
 
     public static void main(String[] args) {
-        System.out.println(characterReplacement("AABABBA", 1));
+        System.out.println(characterReplacementMine("AABABBA", 1));
     }
 }
