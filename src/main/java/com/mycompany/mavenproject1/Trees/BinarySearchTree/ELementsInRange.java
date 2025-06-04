00559a1d799b58f3c8daea5724f5122d91707fe5
Sandeep Singh <ssandeep858@yahoo.com>
2025-06-04 03:06:10 -0700

    BST Question

---
+++ src/main/java/com/mycompany/mavenproject1/Trees/BinarySearchTree/ELementsInRange.java
@@ -0,0 +1,35 @@
+package com.mycompany.mavenproject1.Trees.BinarySearchTree;
+
+public class ELementsInRange {
+
+    /*
+     * Given a Binary Search Tree and two integers k1 and k2, find and print the
+     * elements which are in range k1 and k2 (both inclusive).
+     * 
+     * Print the elements in increasing order.
+     * Sample Input 1:
+     * 8 5 10 2 6 -1 -1 -1 -1 -1 7 -1 -1
+     * 6 10
+     * Sample Output 1:
+     * 6 7 8 10
+     */
+    public static void elementsInRangeK1K2(BinaryTreeNode<Integer> root, int k1, int k2) {
+        
+        //2,4
+        if (root == null) {
+            return;
+        }
+        if(root.data>=k1 && root.data<=k2){
+            System.out.print(root.data +" ");
+        }
+        if(root.data<k2){
+            elementsInRangeK1K2(root.left, k1, k2);
+        }else if(root.data<k1){
+            elementsInRangeK1K2(root.right, k1, k2);
+        }else{
+             elementsInRangeK1K2(root.left, k1, k2);
+             elementsInRangeK1K2(root.right, k1, k2);
+        }
+
+    }
+}
---
+++ src/main/java/com/mycompany/mavenproject1/Trees/BinarySearchTree/SearchInBST.java
@@ -0,0 +1,18 @@
+package com.mycompany.mavenproject1.Trees.BinarySearchTree;
+
+class BinaryTreeNode<T> {
+    T data;
+    BinaryTreeNode<T> left;
+    BinaryTreeNode<T> right;
+
+    public BinaryTreeNode(T data) {
+        this.data = data;
+    }
+}
+
+public class SearchInBST {
+
+    public static boolean searchInBST(BinaryTreeNode<Integer> root, int k) {
+
+    }
+}
