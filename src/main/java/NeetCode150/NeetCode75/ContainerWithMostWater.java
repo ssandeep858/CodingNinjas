c0dfc9be75201d537f69431b47f7d4591361c5c2
Sandeep Singh <ssandeep858@yahoo.com>
2026-01-21 05:35:29 -0800

    revision

--- src/main/java/NeetCode150/NeetCode75/ContainerWithMostWater.java
+++ src/main/java/NeetCode150/NeetCode75/ContainerWithMostWater.java
@@ -7,20 +7,30 @@ package NeetCode150.NeetCode75;
 /**
  *
  * @author ssingh
+ *         You are given an integer array height of length n. There are n
+ *         vertical lines drawn such that the two endpoints of the ith line are
+ *         (i, 0) and (i, height[i]).
+ * 
+ *         Find two lines that together with the x-axis form a container, such
+ *         that the container contains the most water.
+ * 
+ *         Return the maximum amount of water a container can store.
+ * 
+ *         Notice that you may not slant the container.
  */
 public class ContainerWithMostWater {
 
     public static int maxArea(int[] height) {
         int left = 0;
-        int right=height.length-1;
-        
-        int maxArea=0;
-        while(left<right){
-            int area=(right-left)*Math.min(height[left], height[right]);
-            maxArea=Math.max(area, maxArea);
-            if(height[left]<height[right]){
+        int right = height.length - 1;
+
+        int maxArea = 0;
+        while (left < right) {
+            int area = (right - left) * Math.min(height[left], height[right]);
+            maxArea = Math.max(area, maxArea);
+            if (height[left] < height[right]) {
                 left++;
-            }else{
+            } else {
                 right--;
             }
         }
@@ -28,7 +38,7 @@ public class ContainerWithMostWater {
     }
 
     public static void main(String[] args) {
-        int[] input= new int[]{1,8,6,2,5,4,8,3,7};
+        int[] input = new int[] { 1, 8, 6, 2, 5, 4, 8, 3, 7 };
         System.out.println(maxArea(input));
     }
 }
--- src/main/java/NeetCode150/NeetCode75/ThreeSum.java
+++ src/main/java/NeetCode150/NeetCode75/ThreeSum.java
@@ -13,28 +13,33 @@ import java.util.Set;
 /**
  *
  * @author ssingh Given an integer array nums, return all the triplets [nums[i],
- * nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j]
- * + nums[k] == 0.
+ *         nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] +
+ *         nums[j]
+ *         + nums[k] == 0.
  *
- * Notice that the solution set must not contain duplicate triplets.
+ *         Notice that the solution set must not contain duplicate triplets.
  *
  *
  *
- * Example 1:
+ *         Example 1:
  *
- * Input: nums = [-1,0,1,2,-1,-4] Output: [[-1,-1,2],[-1,0,1]] Explanation:
- * nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0. nums[1] + nums[2] + nums[4] =
- * 0 + 1 + (-1) = 0. nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0. The
- * distinct triplets are [-1,0,1] and [-1,-1,2]. Notice that the order of the
- * output and the order of the triplets does not matter.
+ *         Input: nums = [-1,0,1,2,-1,-4] Output: [[-1,-1,2],[-1,0,1]]
+ *         Explanation:
+ *         nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0. nums[1] + nums[2] +
+ *         nums[4] =
+ *         0 + 1 + (-1) = 0. nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0.
+ *         The
+ *         distinct triplets are [-1,0,1] and [-1,-1,2]. Notice that the order
+ *         of the
+ *         output and the order of the triplets does not matter.
  */
 public class ThreeSum {
 
-    //-1,0,1,2,-1,-4
+    // -1,0,1,2,-1,-4
     public static List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
         Set<List<Integer>> set = new HashSet<>();
-        List<List<Integer>> l=new ArrayList<>();
+        List<List<Integer>> l = new ArrayList<>();
         for (int i = 0; i < nums.length; i++) {
             int currentSumToFind = 0 - nums[i];
 
@@ -54,14 +59,13 @@ public class ThreeSum {
                 }
             }
         }
-        //System.out.println(set);
+        // System.out.println(set);
         //System.out.println(Arrays.toString(l.toArray()));
         return new ArrayList<>(set);
     }
 
     public static void main(String[] args) {
-        List<List<Integer>> res
-                = threeSum(new int[]{-1, 0, 1, 2, -1, -4});
+        List<List<Integer>> res = threeSum(new int[] { -1, 0, 1, 2, -1, -4 });
         System.out.println(res);
     }
 }
